c6baceb161c3581bcabbce4b41ed216088667a01
Tim Loden <tim.m.loden@gmail.com>
2019-09-16 12:24:37 -0700

    fixed divi accordion tabbing

--- assets/js/custom/accessibility.js
+++ assets/js/custom/accessibility.js
@@ -1,20 +1,35 @@
 jQuery( document ).ready( function() {
-    
-    var pricingTable = $( 'div.et_pb_pricing_table_wrap' );
 
+	// get pricing tables
+	var pricingTable = $( 'div.et_pb_pricing_table_wrap' );
+
+	// get divi accordions
+	var DiviAccordions = $( 'div.et_pb_accordion' );
+
+	// find pricing tables and add tabindex to links
 	if ( pricingTable.length ) {
-
-		//console.log( pricingTable );
 
 		pricingTable.each( function( index, element ) {
 
-			// Grab each more button control
+			// get linked item
 			var link =  $( element ).find( 'div.et_clickable' );
 
 			link.attr( 'tabIndex', 0 );
 
 		});
-    }
-    
-    
+	}
+
+	if ( DiviAccordions.length ) {
+
+		DiviAccordions.each( function( index, element ) {
+
+			// get main accodrion item
+			var link =  $( element ).find( 'h5.et_pb_toggle_title' );
+
+			link.attr( 'tabIndex', 0 );
+
+		});
+	}
+
+
 });

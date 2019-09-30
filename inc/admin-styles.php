07bb2b836f3c0b35572af7e618a4499a32f24541
Tim Loden <tim.m.loden@gmail.com>
2019-09-30 14:44:35 -0700

    removed dashicons if not logged in

--- inc/admin-styles.php
+++ inc/admin-styles.php
@@ -5,8 +5,9 @@
 
 add_action('admin_head', 'acf_styles');
 
-function acf_styles() {
-  echo '<style>
+function acf_styles()
+{
+    echo '<style>
 	.menu-item-depth-0 [data-name="item_size"], .menu-item-depth-0 [data-name="description"] {
 		display: none;
 	}
@@ -46,74 +47,91 @@ function acf_styles() {
 /* Add icon fonts to admin area
 --------------------------------------------------------------------------------------*/
 
-add_action( 'admin_enqueue_scripts', 'load_admin_style' );
+add_action('admin_enqueue_scripts', 'load_admin_style');
 
-function load_admin_style() {
-	wp_enqueue_style( 'admin_fonts_css', get_template_directory_uri() . '/assets/scss/cagov/cagov.font-only.css', false, '1.0.0' );
+function load_admin_style()
+{
+    wp_enqueue_style('admin_fonts_css', get_template_directory_uri() . '/assets/scss/cagov/cagov.font-only.css', false, '1.0.0');
 }
 
 
 /* Change logo to caweb and check if there is a custom logo
 --------------------------------------------------------------------------------------*/
 
-add_action( 'login_enqueue_scripts', 'custom_login_logo' );
+add_action('login_enqueue_scripts', 'custom_login_logo');
 
-function custom_login_logo() {
-	$general_settings = get_field('general_settings', 'option');
-	$logo = $general_settings['organization_logo'];
-	?>
+function custom_login_logo()
+{
+    $general_settings = get_field('general_settings', 'option');
+    $logo = $general_settings['organization_logo'];
+    ?>
 
-	<style type="text/css">
-		#login h1 a, .login h1 a, body.login div#login h1 a {
-			background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/img/login-logo.png) !important;
-			height: 80px;
-			width: 100%;
-			background-size: auto;
-			background-repeat: no-repeat;
-			background-size: contain;
-			padding-bottom: 0;
-		}
+    <style type="text/css">
+        #login h1 a, .login h1 a, body.login div#login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/img/login-logo.png) !important;
+            height: 80px;
+            width: 100%;
+            background-size: auto;
+            background-repeat: no-repeat;
+            background-size: contain;
+            padding-bottom: 0;
+        }
 
-		.login #nav {
-			font-weight: bold;
-		}
+        .login #nav {
+            font-weight: bold;
+        }
 
-		.login #nav, .login #backtoblog {
-			text-align: center;
-		}
+        .login #nav, .login #backtoblog {
+            text-align: center;
+        }
 
-		<?php if ($logo): ?>
-			#login h1 a, .login h1 a, body.login div#login h1 a {
-				background-image: url(<?php echo($logo['url']); ?>) !important;
-			}
-		<?php endif; ?>
+    <?php if ($logo) : ?>
+            #login h1 a, .login h1 a, body.login div#login h1 a {
+                background-image: url(<?php echo($logo['url']); ?>) !important;
+            }
+    <?php endif; ?>
 
-	</style>
+    </style>
 <?php }
 
 
 /* Change "Logged in as" text
 --------------------------------------------------------------------------------------*/
 
-add_filter( 'admin_bar_menu', 'replace_wordpress_howdy', 25 );
+add_filter('admin_bar_menu', 'replace_wordpress_howdy', 25);
 
-function replace_wordpress_howdy( $wp_admin_bar ) {
-	$my_account = $wp_admin_bar->get_node('my-account');
-	$newtext = str_replace( 'Howdy,', 'Logged in as: ', $my_account->title );
-	$wp_admin_bar->add_node( array(
-		'id' => 'my-account',
-		'title' => $newtext,
-	) );
+function replace_wordpress_howdy( $wp_admin_bar )
+{
+    $my_account = $wp_admin_bar->get_node('my-account');
+    $newtext = str_replace('Howdy,', 'Logged in as: ', $my_account->title);
+    $wp_admin_bar->add_node(
+        array(
+        'id' => 'my-account',
+        'title' => $newtext,
+        ) 
+    );
 }
 
 
 /* Remove admin bar items
 --------------------------------------------------------------------------------------*/
 
-add_action( 'admin_bar_menu', 'remove_wp_logo', 999 );
+add_action('admin_bar_menu', 'remove_wp_logo', 999);
 
-function remove_wp_logo( $wp_admin_bar ) {
-	$wp_admin_bar->remove_node( 'wp-logo' );
-	$wp_admin_bar->remove_node( 'comments' );
+function remove_wp_logo( $wp_admin_bar )
+{
+    $wp_admin_bar->remove_node('wp-logo');
+    $wp_admin_bar->remove_node('comments');
+}
+
+/* Remove dashicons if you are not logged in
+--------------------------------------------------------------------------------------*/
+add_action('wp_enqueue_scripts', 'remove_dashicons');
+
+function remove_dashicons()
+{
+    if (! is_user_logged_in() ) {
+        wp_deregister_style('dashicons');
+    }
 }
 
